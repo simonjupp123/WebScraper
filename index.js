a00bf6bac134723a78c01bdf125838f535208da5
simonjupp123 <101952109+simonjupp123@users.noreply.github.com>
2022-07-02 02:05:44 -0400

    Updating of correct package versions

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const express =  require('express')
 
 const app = express()
 
-app.listen(PORT,  () =>  console.log('server is running on port ${PORT}') )
+app.listen(PORT, () => console.log(`server is running on ${PORT}`))
